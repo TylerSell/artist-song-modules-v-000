f9e5642ee5f5627fa239088c55d03cb1a035b5e6
tylerselldesign <tylerselldesign@gmail.com>
2018-09-26 03:54:21 +0000

    automatically backed up by learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -2,7 +2,7 @@ module Findable
   attr_accessor :name
   
   def find_by_name(name)
-     class_variables.detect{|a| a.name == name}
+     class_variables.all.detect{|a| a.name == name}
   end
   
 end
